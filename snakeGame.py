c7bad536ce9284df3e2fbeece0e4e9867175e9b9
Mathias Kniesel <kniesel1080@gmail.com>
2021-04-09 08:37:13 -0500

    issues with movement again

--- snakeGame.py
+++ snakeGame.py
@@ -8,15 +8,15 @@ w = curses.newwin(sh, sw, 0, 0)
 w.keypad(1)
 w.timeout(100)
 
-snk_x = sw//4
-snk_y = sh//2
+snk_x = sw/4
+snk_y = sh/2
 snake = [
     [snk_y, snk_x],
     [snk_y, snk_x-1],
     [snk_y, snk_x-2]
 ]
 
-food = [sh//2, sw//2]
+food = [sh/2, sw/2]
 w.addch(food[0], food[1], curses.ACS_PI)
 
 key = curses.KEY_RIGHT
@@ -24,11 +24,11 @@ key = curses.KEY_RIGHT
 while True:
     next_key = w.getch()
     key = key if next_key == -1 else next_key
-    '''
+    
     if snake[0][0] in [0, sh] or snake[0][1] in [0, sw] or snake[0] in snake[1:]:
         curses.endwin()
         quit()
-    ''' 
+    
 
     new_head = [snake[0][0], snake[0][1]]
 
@@ -37,9 +37,9 @@ while True:
     if key == curses.KEY_UP:
         new_head[0] -= 1
     if key == curses.KEY_LEFT:
-        new_head[0] -= 1
+        new_head[1] -= 1
     if key == curses.KEY_RIGHT:
-        new_head[0] += 1
+        new_head[1] += 1
 
     snake.insert(0, new_head)
 
@@ -55,4 +55,5 @@ while True:
     else:
         tail = snake.pop()
         w.addch(tail[0], tail[1], ' ')
+
     w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
