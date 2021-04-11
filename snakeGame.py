7e1853fe7f0a3b2ac177a921b56cc8e6087dc40e
Mathias Kniesel <kniesel1080@gmail.com>
2021-04-11 01:24:02 -0500

    changed start length of snake

--- snakeGame.py
+++ snakeGame.py
@@ -20,7 +20,8 @@ def runGame():
         [snk_y, snk_x],
         [snk_y, snk_x-1],
         [snk_y, snk_x-2],
-        [snk_y, snk_x-3]
+        [snk_y, snk_x-3],
+        [snk_y, snk_x-4]
     ]
 
     food = [sh//2, sw//2]
